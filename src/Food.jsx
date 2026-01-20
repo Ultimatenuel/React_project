f5ff6b4ea4afab0d1f3a9394cbd9e07cfc73b1c5
unknown <emmanuelokupa03@gmail.com>
2026-01-20 02:28:54 +0100

    second react commit

---
+++ src/Food.jsx
@@ -0,0 +1,19 @@
+
+function Food(){
+    const food1 = 'orange'
+    const food2 = 'Banana'
+
+    return(
+     <ul>
+        <li> Apple </li>
+        <li></li>
+        <li></li>
+        <li></li>
+    
+      </ul>
+    
+    ); 
+
+}
+    
+export default Food
