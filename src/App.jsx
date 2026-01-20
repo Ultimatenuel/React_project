76dff2d3481dca37c9b5167897d3b56060e3a87e
unknown <emmanuelokupa03@gmail.com>
2026-01-20 18:25:36 +0100

    proptypes

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,15 @@ function App () {
   
   return (
       <>
-          < Student name="Spongebob" age={19} isStudent={true}/>
-       < Student name="Patric" age={18} isStudent={true}/>
+      < Student name="Spongebob" age="19" isStudent={true}/>
+      < Student name="Patric" age={18} isStudent={true}/>
        <Student name="Belligham" age={19} isStudent={true}/>
+        <Student name="andy" age={19} isStudent={true}/>
+        
+    
+           
+    
+
        </> 
   ); 
 }
--- src/Student.jsx
+++ src/Student.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types'
 
 
 function Student(props) {
@@ -10,5 +11,10 @@ function Student(props) {
     ); 
   
 }
-    
+Student.PropTypes ={
+  name: PropTypes.string,
+  age: PropTypes.number,
+  isStudent: PropTypes.bool,
+
+} 
 export default Student
