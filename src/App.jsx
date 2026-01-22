69f3944e236416006b6511b9c0f017f83ec6d7ea
unknown <emmanuelokupa03@gmail.com>
2026-01-22 11:34:56 +0100

    to-do-list

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,11 @@
+import TodoList from "./TodoList.jsx";
 
-import Student from './Student.jsx'
-
-function App () {
-  
+function App() {
   return (
-      <>
-      < Student name="Spongebob" age="19" isStudent={true}/>
-      < Student name="Patric" age={18} isStudent={true}/>
-       <Student name="Belligham" age={19} isStudent={true}/>
-        <Student name="andy" age={19} isStudent={true}/>
-        
-    
-           
-    
-
-       </> 
-  ); 
+    <>
+      <TodoList />
+    </>
+  );
 }
 
-export default App
+export default App;
